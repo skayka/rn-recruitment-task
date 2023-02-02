2adf6e1335da4b001f14120fffa8e38190032bf0
Oleg Novak <oleg.novak@gmail.com>
2023-02-02 13:49:23 +0200

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  printWidth: 120,
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+};
