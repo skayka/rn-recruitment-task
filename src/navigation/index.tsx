3d9df9be8534f3e47c4c9bdf2259b515595a113b
Oleg Novak <oleg.novak@gmail.com>
2023-02-06 14:26:43 +0200

    Update navigation file structure

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,29 +1,4 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack';
-import PokemonList from '../screens/PokemonList';
-import PokemonDetails from '../screens/PokemonDetails/PokemonDetailsScreen';
-export type StackNavigatorParamList = {
-  PokemonList: undefined;
-  PokemonDetails: { url: string };
-};
+import Navigation from './AppNavigation';
 
-type PokemonDetailsProps = NativeStackScreenProps<StackNavigatorParamList, 'PokemonDetails'>;
-const Stack = createNativeStackNavigator<StackNavigatorParamList>();
-
-const Navigation = (): JSX.Element => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="PokemonList">
-        <Stack.Screen name="PokemonList" component={PokemonList} options={{ headerShown: false }} />
-        <Stack.Screen
-          name="PokemonDetails"
-          component={PokemonDetails}
-          options={{ headerTitle: '', headerBackTitle: '', headerTransparent: true }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
+export * from './AppNavigation';
 export default Navigation;
